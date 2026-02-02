96bef2130a794da217e2837dff329b71f381db3a
ap-pamungkas <agustinusputra94@gmail.com>
2026-02-02 16:23:19 +0700

    feat: Implement Prisma client setup with Accelerate extension and add Prisma generation/migration scripts.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,13 @@
 import { PrismaClient } from "@/generated/prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,
-});
+const globalForPrisma = global as unknown as { prisma: any };
+
+// Inisialisasi dengan memaksa tipe 'any' pada constructor untuk melewati validasi Subset
+export const prisma =
+  globalForPrisma.prisma ||
+  (new (PrismaClient as any)({})).$extends(withAccelerate());
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
