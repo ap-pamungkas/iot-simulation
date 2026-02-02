8d8633bfe57f4a01d9d5bd12c1dd66c5e41b917b
ap-pamungkas <agustinusputra94@gmail.com>
2026-02-02 15:53:41 +0700

    feat: add Prisma client initialization for database access.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,7 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
 
 const prisma = new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL!,
+  accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,
 });
 
 export default prisma;
