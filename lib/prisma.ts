f7d2aff5225e7cdc2ec3000252f51ce6fae9f282
ap-pamungkas <agustinusputra94@gmail.com>
2026-02-02 16:03:01 +0700

    feat: Initialize and export Prisma client with PrismaPg adapter and global singleton pattern.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,11 @@
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '@/generated/prisma/client'
 
-const prisma = new PrismaClient({
-  accelerateUrl: process.env.PRISMA_ACCELERATE_URL!,
-});
+const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! })
+const prisma = new PrismaClient({ adapter: pool })
 
-export default prisma;
+const globalForPrisma = global as unknown as { prisma: typeof prisma }
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+export default prisma
