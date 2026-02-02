822ae7bf4b2ce89ca851108997a7fdb6cbc19e02
ap-pamungkas <agustinusputra94@gmail.com>
2026-02-02 16:49:59 +0700

    feat: Add Prisma client initialization and export for database access.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,7 @@
 import { PrismaClient } from "@/generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as { prisma: any };
-
-// Inisialisasi tanpa argumen karena file kustom kamu menolak 'datasources'
-// Prisma akan otomatis mencari variabel lingkungan DATABASE_URL
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient().$extends(withAccelerate());
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+const prisma = new PrismaClient({
+  accelerateUrl: process.env.DATABASE_URL!,
+});
 
 export default prisma;
